8209b9bf47c9c6802afc784d46ee4df649565ef7
Leonid Beschastny <lbeschastny@iknow.travel>
2014-09-24 13:35:26 +0400

    Updated mpromise version

--- test/promise.test.js
+++ test/promise.test.js
@@ -11,29 +11,29 @@ describe('promise', function () {
   it('Promise#error', function () {
     var p = new Promise()
     p.error(function(){});
-    // 2 due to internal event listener
-    expect(p.listeners('error').length).to.be(2);
+    // 3 due to internal event listener
+    expect(p.emitter.listeners('reject').length).to.be(3);
   });
 
   it('Promise#success', function () {
     var p = new Promise()
     p.success(function(){});
-    // 2 due to internal event listener
-    expect(p.listeners('success').length).to.be(2);
+    // 3 due to internal event listener
+    expect(p.emitter.listeners('fulfill').length).to.be(3);
   });
 
   it('Promise#complete', function () {
     var p = new Promise()
     p.complete(function(){});
-    // 2 due to internal event listener
-    expect(p.listeners('error').length).to.be(2);
-    expect(p.listeners('success').length).to.be(2);
+    // 3 due to internal event listener
+    expect(p.emitter.listeners('reject').length).to.be(3);
+    expect(p.emitter.listeners('fulfill').length).to.be(3);
   });
 
   it('Promise#each', function () {
     var p = new Promise()
     p.each(function(){});
-    expect(p.listeners('each').length).to.be(1);
+    expect(p.emitter.listeners('each').length).to.be(1);
   });
 
   it('Promise#then', function () {
